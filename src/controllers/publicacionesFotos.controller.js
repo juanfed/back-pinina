c7d968e178caf59af2ba5fd341beb73086effad2
Juan Ospina <juan0806lv@gmail.com>
2022-04-07 13:44:41 -0500

    metodo para subir fotos de una publciacion al ser creada

--- src/controllers/publicacionesFotos.controller.js
+++ src/controllers/publicacionesFotos.controller.js
@@ -4,7 +4,7 @@ const mostrarFotos = async (req) => {
 
     try {
 
-        const id_publicacion  = req.params.id_publicacion;
+        const id_publicacion = req.params.id_publicacion;
 
 
         let respuesta = await pool.query(`select * from f_fotos_publicacion_id(${id_publicacion})`);
@@ -19,9 +19,41 @@ const mostrarFotos = async (req) => {
 
     } catch (error) {
         console.log(error)
-        throw new Error (`Error en mostrarFotos ${error}`)
+        throw new Error(`Error en mostrarFotos ${error}`)
     }
 
 }
 
-module.exports = {mostrarFotos};
+
+
+const subirFotoPublicacion = async (id_publicacion, id_mascotas, req) => {
+    try {
+        if (!req.files || Object.keys(req.files).length === 0 || !req.files.imagen) {
+            return null;
+
+        } else {
+            let out;
+            for (let i = 0; i < req.files.length; i++) {
+                const { imagen } = req.files[i];
+                const uploadPath = path.join('src/uploads/uploads2/', imagen.name);
+                const nombre_imagen = imagen.name;
+                imagen.mv(uploadPath, (err) => {
+                    if (err) {
+                        throw new Error("Error subiendo imagen " + err)
+                    }
+                    console.log('File uploaded to ' + uploadPath);
+                })
+                const imagenSubida = await pool.query('select * from f_insert_foto_publicacion($1,$2,$3,$4)',[uploadPath, nombre_imagen, id_mascotas, id_publicacion]);
+                out += imagenSubida;
+            }
+            return out;
+        }
+
+
+
+    } catch (error) {
+        throw new Error('Error subirFotoPublicacion ' + error);
+    }
+}
+
+module.exports = { mostrarFotos };
